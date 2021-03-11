5a4c33ff0b64783fdb0b2445e830692351d0d253
LucasGooes <lucasmarcos343@gmail.com>
2021-03-11 19:03:03 -0400

    inicial commit

---
+++ src/main/java/com/marinhosoftware/hotelurbano/HotelUrbanoApplication.java
@@ -0,0 +1,13 @@
+package com.marinhosoftware.hotelurbano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelUrbanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelUrbanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marinhosoftware/hotelurbano/HotelUrbanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinhosoftware.hotelurbano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelUrbanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
