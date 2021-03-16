2064dcfca952f588ef1af07d134cbb44606971f7
LucasGooes <lucasmarcos343@gmail.com>
2021-03-16 13:48:27 -0400

    Servico - hashcode and equals

--- src/main/java/com/marinhosoftware/hotelurbano/domain/Servico.java
+++ src/main/java/com/marinhosoftware/hotelurbano/domain/Servico.java
@@ -63,7 +63,31 @@ public class Servico implements Serializable {
 	public void setReserva(Reserva reserva) {
 		this.reserva = reserva;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servico other = (Servico) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 	
 }
