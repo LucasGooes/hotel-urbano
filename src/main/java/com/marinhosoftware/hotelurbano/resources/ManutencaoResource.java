603f0e0e5fd7418f3fe32c369d0c08a701faaa7b
LucasGooes <lucasmarcos343@gmail.com>
2021-04-09 17:12:54 -0400

    Listando todas as manutenções

--- src/main/java/com/marinhosoftware/hotelurbano/resources/ManutencaoResource.java
+++ src/main/java/com/marinhosoftware/hotelurbano/resources/ManutencaoResource.java
@@ -1,6 +1,7 @@
 package com.marinhosoftware.hotelurbano.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,14 +34,13 @@ public class ManutencaoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	/*
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<ManutencaoDTO>> findAll() {
+	public ResponseEntity<List<Manutencao/*DTO*/>> findAll() {
 		List<Manutencao> list = service.findAll();
-		List<ManutencaoDTO> listDto = list.stream()
+		/*List<ManutencaoDTO> listDto = list.stream()
 				.map(obj -> new ManutencaoDTO(obj))
-				.collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);
+				.collect(Collectors.toList());*/
+		return ResponseEntity.ok().body(list/*Dto*/);
 	}
-	*/
 }
