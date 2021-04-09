4a30a1c255d6e4d8c93e11044697da3e699aa735
LucasGooes <lucasmarcos343@gmail.com>
2021-04-09 17:31:48 -0400

    Iniciando a implementação do Service e Resource de Reserva

--- src/main/java/com/marinhosoftware/hotelurbano/resources/ReservaResource.java
+++ src/main/java/com/marinhosoftware/hotelurbano/resources/ReservaResource.java
@@ -1,11 +1,15 @@
 package com.marinhosoftware.hotelurbano.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marinhosoftware.hotelurbano.domain.Reserva;
 import com.marinhosoftware.hotelurbano.services.ReservaService;
@@ -22,5 +26,37 @@ public class ReservaResource {
 		Reserva obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Reserva obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	/*
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody ReservaDTO objDto, @PathVariable Integer id) {
+		Reserva obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}*/
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Reserva> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	/*
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ReservaDTO>> findAll() {
+		List<Reserva> list = service.findAll();
+		List<ReservaDTO> listDto = list.stream()
+				.map(obj -> new ReservaDTO(obj))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}*/
 
 }
--- src/main/java/com/marinhosoftware/hotelurbano/services/ReservaService.java
+++ src/main/java/com/marinhosoftware/hotelurbano/services/ReservaService.java
@@ -1,12 +1,16 @@
 package com.marinhosoftware.hotelurbano.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marinhosoftware.hotelurbano.domain.Reserva;
 import com.marinhosoftware.hotelurbano.repositories.ReservaRepository;
+import com.marinhosoftware.hotelurbano.serivces.exceptions.DataIntegrityException;
 import com.marinhosoftware.hotelurbano.serivces.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +24,41 @@ public class ReservaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Reserva.class.getName()));
 	}
+	
+	@Transactional
+	public Reserva insert(Reserva obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		return obj;
+	}
+	
+	/*
+	public Reserva update(Reserva obj) {
+		Reserva newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	*/
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("NÃO É POSSÍVEL EXCLUIR UM CLIENTE QUE POSSUI RESERVAS REGISTRADAS");
+		}
+	}
+	
+	public List<Reserva> findAll() {
+		return repo.findAll();
+	}
+	/*
+	public Reserva fromDTO(ReservaDTO objDto) {
+		return new Reserva(null, objDto.getNome(), objDto.getRg(), objDto.getCpf(), objDto.getEmail(), Sexo.toEnum(objDto.getSexo()));
+	}
 
+	private void updateData(Reserva newObj, Reserva obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	*/
 }
